ed3a254bbb5a413ba99df7dc8c1c15c85c90956f
raihanM95 <mahmud.raihan@yahoo.com>
2021-09-26 11:34:35 +0600

    Generate OTP

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OTP
+{
+    class Program
+    {
+        // Generate OTP
+        public string GenerateRandomOTP(int iOTPLength, string[] saAllowedCharacters)
+        {
+            string sOTP = String.Empty;
+            string sTempChars = String.Empty;
+
+            Random rand = new Random();
+
+            for (int i = 0; i < iOTPLength; i++)
+            {
+                int p = rand.Next(0, saAllowedCharacters.Length);
+                sTempChars = saAllowedCharacters[rand.Next(0, saAllowedCharacters.Length)];
+                sOTP += sTempChars;
+            }
+            return sOTP;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
